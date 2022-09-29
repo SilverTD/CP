a4fa3c02301294f3764f15ac22655bbac4a30032
Thanh Duy <55396370+D11227@users.noreply.github.com>
2022-09-29 19:03:21 +0700

    Create REVERSE_BITS.cpp

---
+++ REVERSE_BITS.cpp
@@ -0,0 +1,27 @@
+#pragma GCC target("avx2")
+#pragma GCC optimize("Ofast")
+
+#include <iostream>
+#include <bitset>
+#include <utility>
+#include <ciso646>
+
+template<typename T> constexpr std::size_t get_max_bits() { return sizeof(T) * 8; }
+template<typename T, std::size_t MAX_BITS = get_max_bits<T>()>
+[[nodiscard]] std::pair<unsigned, std::bitset<MAX_BITS>> reverse_bit(const T& n) {
+        T reverse_num = 0;
+
+        for (unsigned i = 0; i < MAX_BITS; ++i)
+                if ((n bitand (1 << i)))
+                        reverse_num or_eq 1 << ((MAX_BITS - 1) - i);
+
+        return { reverse_num, std::bitset<MAX_BITS>(reverse_num) };
+}
+
+int main() {
+        short n = 522;
+        auto [number, bits] = reverse_bit<short>(n);
+        std::cout << n << "\t" << std::bitset<get_max_bits<short>()>(n) << '\n';
+        std::cout << number << "\t" << bits << '\n';
+        return 0;
+}
