910c12e507bb1f7a6dcc78fe68d2f2f06877db06
Thanh Duy <55396370+D11227@users.noreply.github.com>
2022-10-08 22:44:15 +0700

    Update Solution.cpp

--- MATH/X^K + Y^K ?/Solution.cpp
+++ MATH/X^K + Y^K ?/Solution.cpp
@@ -61,7 +61,7 @@ struct Fraction {
 };
 
 int main() {
-       std::ios_base::sync_with_stdio(false);
+        std::ios_base::sync_with_stdio(false);
         std::cin.tie(0);
         std::cout.tie(0);
 
