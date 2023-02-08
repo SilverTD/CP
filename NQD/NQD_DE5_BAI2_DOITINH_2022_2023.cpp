67041895bfb18daebc6b80d7a1ec6a5f4bf37c13
Thanh Duy <sologame37@gmail.com>
2023-02-08 23:06:48 +0700

    Update NQD_DE5_BAI2_DOITINH_2022_2023.cpp

--- NQD/NQD_DE5_BAI2_DOITINH_2022_2023.cpp
+++ NQD/NQD_DE5_BAI2_DOITINH_2022_2023.cpp
@@ -15,6 +15,10 @@
 #include <algorithm>
 
 int32_t main() {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(0);
+        std::cout.tie(0);
+
         int n, count = 0;
         std::cin >> n;
         std::vector<in32_t> b(n);
